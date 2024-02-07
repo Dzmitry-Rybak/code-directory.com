d95eba987966900929f74e1f5793a131114801b7
Dzmitry Rybak <nakadisa@icloud.com>
2024-02-07 15:27:06 +0000

    robots.txt

--- next/src/app/dashboard/page.jsx
+++ next/src/app/dashboard/page.jsx
@@ -2,6 +2,9 @@ import Dashboard from "@/app/components/dashboard/dashboard"
 
 export const metadata = {
     title: 'Dashboard',
+    robots: {
+        index: false,
+      },
 };
 
 const DashboardPage = () => {
