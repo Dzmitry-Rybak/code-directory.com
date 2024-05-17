f87ab44d81a391732a4a8f3a911fad507064bc8a
Dzmitry Rybak <nakadisa@icloud.com>
2024-05-17 16:45:55 +0000

    updates for react native

--- next/src/config/config.jsx
+++ next/src/config/config.jsx
@@ -1,7 +1,7 @@
 const config = {
     apiUrl: process.env.REACT_APP_API_URL || 'http://167.99.243.185:5000',
     socialLinks: {
-        instagram: 'https://www.instagram.com/dymitr.rybak',
+        instagram: 'https://www.instagram.com/code_directory/',
         telegram: 'https://t.me/dzmitryrybak',
         patreon: 'https://patreon.com/'
     },
