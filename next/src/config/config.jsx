4ed5c6ee0593999841fda89b501db4efef0a173f
Dzmitry Rybak <nakadisa@icloud.com>
2024-05-26 12:45:00 +0200

    .env, config

--- next/src/config/config.jsx
+++ next/src/config/config.jsx
@@ -1,5 +1,5 @@
 const config = {
-    apiUrl: process.env.REACT_APP_API_URL || 'http://172.20.10.11:5001',
+    apiUrl: process.env.REACT_APP_API_URL || 'http://167.99.243.185:5000',
     socialLinks: {
         instagram: 'https://www.instagram.com/code_directory/',
         telegram: 'https://t.me/dzmitryrybak',
